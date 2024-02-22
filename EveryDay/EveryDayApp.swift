4254a044618efdfea44712d10fc3e29cd91a5ef0
Chris Melamed <cgmelamed@gmail.com>
2024-02-21 23:20:30 -0500

    Initial Commit

---
+++ EveryDay/EveryDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryDayApp.swift
+//  EveryDay
+//
+//  Created by Chris Melamed on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EveryDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
